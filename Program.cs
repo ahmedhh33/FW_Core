9dd56f3b02ff168f71d736bf9c3bff02e7191323
ahmedhh33 <ahmedhh3633@gmail.com>
2023-09-18 14:27:57 +0400

    do migration for DDL and how to do DML

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Entitiyframework
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var _context = new ApplicationDBContext();
+
+            var emp3 = new Employee { Name = "ahmed" };
+            var emp4 = new Employee { Name = "saleh" };
+
+            _context.Employees.Add(emp3);
+            _context.Employees.Add(emp4);
+
+            _context.SaveChanges();
+
+            var emp5 = _context.Employees.Where( x=>x.Id == 1 ).First();
+            Console.WriteLine(emp5.Name);
+            
+
+            emp5.Name = "nasser";
+            _context.Employees.Update(emp5);
+            _context.SaveChanges();
+
+            var emp6 = _context.Employees.First();
+            Console.WriteLine(emp5.Name);
+
+        }
+    }
+}
